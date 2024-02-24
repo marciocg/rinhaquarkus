43b688886aa3e0255b8cf93e99b000a27890b2c8
Márcio C.G. <marcio.goulart@gmail.com>
2024-02-24 18:36:13 -0300

    Inclui anotação @DisabledOnIntegrationTest para retirar teste no momento do build nativo, pois usando container linux não roda no windows o teste

--- src/test/java/io/github/marciocg/clientes/ClientesResourceTestIT.java
+++ src/test/java/io/github/marciocg/clientes/ClientesResourceTestIT.java
@@ -1,8 +1,10 @@
 package io.github.marciocg.clientes;
 
+import io.quarkus.test.junit.DisabledOnIntegrationTest;
 import io.quarkus.test.junit.QuarkusIntegrationTest;
 
 @QuarkusIntegrationTest
+@DisabledOnIntegrationTest
 public class ClientesResourceTestIT extends ClientesResourceTest {
 
 
