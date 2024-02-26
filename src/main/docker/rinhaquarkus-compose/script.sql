9aa3768635480bf823399f8bdbe0b4cd2ba61245
Márcio C.G. <marcio.goulart@gmail.com>
2024-02-26 14:54:23 -0300

    Ajusta docker-compose e script sql para rodar com container:v1.2

--- src/main/docker/rinhaquarkus-compose/script.sql
+++ src/main/docker/rinhaquarkus-compose/script.sql
@@ -4,18 +4,17 @@
     create sequence transacoes_SEQ start with 1 increment by 50;
 
     create table saldo (
+        id integer not null,
         limite integer,
         total integer,
-        version integer,
-        id bigint not null,
         primary key (id)
     );
 
     create table transacoes (
         id integer not null,
+        saldo_id integer,
         tipo varchar(1),
         valor integer,
-        saldo_id bigint,
         descricao varchar(255),
         realizadaEm varchar(255),
         primary key (id)
@@ -25,8 +24,8 @@
        add constraint FK5glwy1bhv9crnua3fowa6ursf 
        foreign key (saldo_id) 
        references saldo;
-insert into saldo (id, total, limite) values (1, 0, 100000);
-insert into saldo (id, total, limite) values (2, 0, 80000);
-insert into saldo (id, total, limite) values (3, 0, 1000000);
-insert into saldo (id, total, limite) values (4, 0, 10000000);
-insert into saldo (id, total, limite) values (5, 0, 500000);
+insert into saldo (id, total, limite) values (1, 0, 100000, 1);
+insert into saldo (id, total, limite) values (2, 0, 80000, 1);
+insert into saldo (id, total, limite) values (3, 0, 1000000, 1);
+insert into saldo (id, total, limite) values (4, 0, 10000000, 1);
+insert into saldo (id, total, limite) values (5, 0, 500000, 1);
