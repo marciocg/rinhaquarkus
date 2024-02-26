1d5017767f4045f4530b988ad5526f38fb4fc89f
Márcio C.G. <marcio.goulart@gmail.com>
2024-02-26 16:05:21 -0300

    Ajusta script.sql

--- src/main/docker/rinhaquarkus-compose/script.sql
+++ src/main/docker/rinhaquarkus-compose/script.sql
@@ -24,8 +24,8 @@
        add constraint FK5glwy1bhv9crnua3fowa6ursf 
        foreign key (saldo_id) 
        references saldo;
-insert into saldo (id, total, limite) values (1, 0, 100000, 1);
-insert into saldo (id, total, limite) values (2, 0, 80000, 1);
-insert into saldo (id, total, limite) values (3, 0, 1000000, 1);
-insert into saldo (id, total, limite) values (4, 0, 10000000, 1);
-insert into saldo (id, total, limite) values (5, 0, 500000, 1);
+insert into saldo (id, total, limite) values (1, 0, 100000);
+insert into saldo (id, total, limite) values (2, 0, 80000);
+insert into saldo (id, total, limite) values (3, 0, 1000000);
+insert into saldo (id, total, limite) values (4, 0, 10000000);
+insert into saldo (id, total, limite) values (5, 0, 500000);
