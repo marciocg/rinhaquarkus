055928c12d3a28acfa9aa52fdad35bf52fc9ff76
Márcio C.G. <marcio.goulart@gmail.com>
2024-02-26 08:23:29 -0300

    ajusta import.sql

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,31 +1,7 @@
-insert into saldo (id, total, limite) values (1, 0, 100000);
-insert into saldo (id, total, limite) values (2, 0, 80000);
-insert into saldo (id, total, limite) values (3, 0, 1000000);
-insert into saldo (id, total, limite) values (4, 0, 10000000);
-insert into saldo (id, total, limite) values (5, 0, 500000);
-
---insert into rinha.saldo (id, total, limite) values (1, 0, 100000);
---insert into rinha.saldo (id, total, limite) values (2, 0, 80000);
---insert into rinha.saldo (id, total, limite) values (3, 0, 1000000);
---insert into rinha.saldo (id, total, limite) values (4, 0, 10000000);
---insert into rinha.saldo (id, total, limite) values (5, 0, 500000);
-
---insert into Transacoes (id, valor, tipo, descricao, realizadaEm, saldo_id) values (1, 0, 'c', 'inicial', CURRENT_TIMESTAMP, 1);
---insert into Transacoes (id, valor, tipo, descricao, realizadaEm, saldo_id) values (2, 0, 'c', 'inicial', CURRENT_TIMESTAMP, 2);
---insert into Transacoes (id, valor, tipo, descricao, realizadaEm, saldo_id) values (3, 0, 'c', 'inicial', CURRENT_TIMESTAMP, 3);
---insert into Transacoes (id, valor, tipo, descricao, realizadaEm, saldo_id) values (4, 0, 'c', 'inicial', CURRENT_TIMESTAMP, 4);
---insert into Transacoes (id, valor, tipo, descricao, realizadaEm, saldo_id) values (5, 0, 'c', 'inicial', CURRENT_TIMESTAMP, 5);
-
---insert into Transacoes (id, valor, tipo, descricao, realizadaEm, saldoId) values (1, 0, 'c', 'inicial', CURRENT_TIMESTAMP, 1);
---insert into Transacoes (id, valor, tipo, descricao, realizadaEm, saldoId) values (2, 0, 'c', 'inicial', CURRENT_TIMESTAMP, 2);
---insert into Transacoes (id, valor, tipo, descricao, realizadaEm, saldoId) values (3, 0, 'c', 'inicial', CURRENT_TIMESTAMP, 3);
---insert into Transacoes (id, valor, tipo, descricao, realizadaEm, saldoId) values (4, 0, 'c', 'inicial', CURRENT_TIMESTAMP, 4);
---insert into Transacoes (id, valor, tipo, descricao, realizadaEm, saldoId) values (5, 0, 'c', 'inicial', CURRENT_TIMESTAMP, 5);
-
---insert into Transacoes (id, valor, tipo, descricao, realizadaEm) values (1, 0, 'c', 'inicial', CURRENT_TIMESTAMP);
---insert into Transacoes (id, valor, tipo, descricao, realizadaEm) values (2, 0, 'c', 'inicial', CURRENT_TIMESTAMP);
---insert into Transacoes (id, valor, tipo, descricao, realizadaEm) values (3, 0, 'c', 'inicial', CURRENT_TIMESTAMP);
---insert into Transacoes (id, valor, tipo, descricao, realizadaEm) values (4, 0, 'c', 'inicial', CURRENT_TIMESTAMP);
---insert into Transacoes (id, valor, tipo, descricao, realizadaEm) values (5, 0, 'c', 'inicial', CURRENT_TIMESTAMP);
+insert into saldo (id, total, limite, version) values (1, 0, 100000, 1);
+insert into saldo (id, total, limite, version) values (2, 0, 80000, 1);
+insert into saldo (id, total, limite, version) values (3, 0, 1000000, 1);
+insert into saldo (id, total, limite, version) values (4, 0, 10000000, 1);
+insert into saldo (id, total, limite, version) values (5, 0, 500000, 1);
 
 --select column_name, data_type, character_maximum_length, column_default, is_nullable from INFORMATION_SCHEMA.COLUMNS where table_name = 'Transacoes';
